bbbf634f23911d9a9a850a4db9d7b6fd79d1bb02
Shakiqur Rahman <106921839+Shakiqurrahman@users.noreply.github.com>
2025-01-16 21:42:16 +0600

    new button added

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        test: "text-primary hover:underline",
         login: "bg-white text-primary hover:bg-primary hover:text-primary-foreground border border-primary text-base"
       },
       size: {
